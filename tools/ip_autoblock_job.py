a6592338792146365c0ece54845ea94c59de464d
cmyui <cmyuiosu@gmail.com>
2024-07-20 09:59:15 -0400

    rewrite and improve some things

--- tools/ip_autoblock_job.py
+++ tools/ip_autoblock_job.py
@@ -3,7 +3,6 @@ import re
 from collections import defaultdict
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Any
 
 import settings
 from adapters import cloudflare
@@ -15,20 +14,8 @@ from adapters import datadog
 INVALID_QUERY_PARAM_URL_REGEX_PATTERN = r"^/[^?]*&.*$"
 
 
-def is_ddos_request(log_attributes: dict[str, Any]) -> bool:
-    # look for urls like /abc&def=1
-    url_match = re.match(
-        INVALID_QUERY_PARAM_URL_REGEX_PATTERN,
-        log_attributes["http"]["url"],
-    )
-    if not url_match:
-        return False
-
-    return True
-
-
 @dataclass
-class DDoSRequest:
+class NetworkRequest:
     ip: ipaddress.IPv4Address | ipaddress.IPv6Address
     http_method: str
     http_host: str
@@ -37,6 +24,17 @@ class DDoSRequest:
     http_referrer: str
     http_user_agent: str
     timestamp: datetime
+
+    def is_ddos_request(self) -> bool:
+        # look for urls like /abc&def=1
+        url_match = re.match(
+            INVALID_QUERY_PARAM_URL_REGEX_PATTERN,
+            self.http_url,
+        )
+        if not url_match:
+            return False
+
+        return True
 
 
 def main() -> int:
@@ -47,41 +45,42 @@ def main() -> int:
     )
     print(f"Received {len(datadog_logs)} logs from Datadog")
 
-    ddos_requests_by_ip: defaultdict[str, list[DDoSRequest]] = defaultdict(list)
+    malicious_requests_by_ip: defaultdict[str, list[NetworkRequest]] = defaultdict(list)
     for datadog_log in datadog_logs:
-        log_attributes = datadog_log["attributes"].get("attributes")
-        if log_attributes is None:
+        log = datadog_log["attributes"].get("attributes")
+        if log is None:
             continue  # non-access log
 
-        if is_ddos_request(log_attributes):
-            ddos_request = DDoSRequest(
-                ip=ipaddress.ip_address(log_attributes["network"]["client"]["ip"]),
-                http_method=log_attributes["http"]["method"],
-                http_host=log_attributes["http"]["host"],
-                http_url=log_attributes["http"]["url"],
-                http_status_code=int(log_attributes["http"]["status_code"]),
-                http_referrer=log_attributes["http"]["referrer"],
-                http_user_agent=log_attributes["http"]["user_agent"],
-                timestamp=datetime.fromtimestamp(log_attributes["date"] / 1000),
-            )
-            ddos_requests_by_ip[log_attributes["network"]["client"]["ip"]].append(
-                ddos_request
-            )
+        request = NetworkRequest(
+            ip=ipaddress.ip_address(log["network"]["client"]["ip"]),
+            http_method=log["http"]["method"],
+            http_host=log["http"]["host"],
+            http_url=log["http"]["url"],
+            http_status_code=int(log["http"]["status_code"]),
+            http_referrer=log["http"]["referrer"],
+            http_user_agent=log["http"]["user_agent"],
+            timestamp=datetime.fromtimestamp(log["date"] / 1000),
+        )
+
+        if request.is_ddos_request():
+            malicious_requests_by_ip[log["network"]["client"]["ip"]].append(request)
         else:
             pass  # print("Normal request", log_attributes["http_url"])
     print(f"Parsed {len(datadog_logs)} requests from logs")
 
-    ipv4_addresses: list[str] = [
+    malicious_ipv4_addresses: list[str] = [
         ip
-        for ip in ddos_requests_by_ip
+        for ip in malicious_requests_by_ip
         if isinstance(ipaddress.ip_address(ip), ipaddress.IPv4Address)
     ]
 
     cloudflare.add_list_items(
         list_id=settings.CLOUDFLARE_DDOS_IPS_LIST_ID,
-        items=ipv4_addresses,
+        items=malicious_ipv4_addresses,
     )
-    print(f"Added {len(ipv4_addresses)} IPs to Cloudflare block list")
+    print(
+        f"Added {len(malicious_ipv4_addresses)} malicious IPs to Cloudflare block list"
+    )
 
     # TODO: auto-report ips to abuseipdb
 
