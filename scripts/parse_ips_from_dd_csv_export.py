c7557f48524d1ef3cecefea93c3c6bf7f3abbc16
cmyui <cmyuiosu@gmail.com>
2024-07-20 02:39:34 -0400

    cleanup

--- scripts/parse_ips_from_dd_csv_export.py
+++ scripts/parse_ips_from_dd_csv_export.py
@@ -2,14 +2,13 @@ import csv
 from typing import Any
 
 import re
-from pygrok import Grok
 
-CSV_FILEPATH = ".data/extract-2024-07-20T01_32_26.744Z.csv"
-
-# NGINX_ACCESS_LOG_GROK_PATTERN = r'\[%{date("dd/MMM/yyyy:HH:mm:ss Z"):date}\]\s+%{ip:network.client.ip}\s+-\s+"%{word:http.method}\s+%{notSpace:http.host}\s+%{notSpace:http.url}"\s+%{integer:http.status_code}\s+%{integer:network.body_bytes_sent}\s+"%{notSpace:http.referrer}"\s+"%{data:http.user_agent}"'
-
+CSV_FILEPATH = ".data/extract-2024-07-20T03_46_25.084Z.csv"
 
 NGINX_ACCESS_LOG_REGEX_PATTERN = r'\[(?P<date>\d{2}/\w{3}/\d{4}:\d{2}:\d{2}:\d{2} \+\d{4})\]\s+(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+-\s+"(?P<http_method>\w+)\s+(?P<http_host>\S+)\s+(?P<http_url>\S+)"\s+(?P<http_status_code>\d{3})\s+(?P<network_body_bytes_sent>\d+)\s+"(?P<http_referrer>\S+)"\s+"(?P<http_user_agent>.+)"'
+
+# Find urls starting their query string with & and not ?
+INVALID_QUERY_PARAM_URL_REGEX_PATTERN = r"^/[^?]*&.*$"
 
 
 def parse_nginx_access_log(val: str) -> dict[str, Any]:
@@ -20,13 +19,11 @@ def parse_nginx_access_log(val: str) -> dict[str, Any]:
         raise ValueError(f"Failed to parse: {val}")
 
 
-# Find urls starting their query string with & and not ?
-INVALID_QUERY_PARAM_URL_REGEX_PATTERN = r".*&[^?].*"
-
-
 def is_ddos_request(log_attributes: dict[str, Any]) -> bool:
+    # look for urls like /abc&def=1
     url_match = re.match(
-        INVALID_QUERY_PARAM_URL_REGEX_PATTERN, log_attributes["http_url"]
+        INVALID_QUERY_PARAM_URL_REGEX_PATTERN,
+        log_attributes["http_url"],
     )
     if not url_match:
         return False
@@ -36,7 +33,7 @@ def is_ddos_request(log_attributes: dict[str, Any]) -> bool:
 
 def main() -> int:
     failed_to_parse = 0
-    ips = set()
+    deduplicated_ips: set[str] = set()
     with open(CSV_FILEPATH) as f:
         csv_reader = csv.DictReader(f)
         for line in csv_reader:
@@ -47,16 +44,23 @@ def main() -> int:
                 continue
 
             if is_ddos_request(log_attributes):
-                ips.add(log_attributes["ip"])
-                print("DDOS request detected", log_attributes["http_url"])
+                deduplicated_ips.add(log_attributes["ip"])
+                print(
+                    "DDOS request detected",
+                    log_attributes["http_method"],
+                    log_attributes["http_url"],
+                )
+                # TODO: auto-report to abuseipdb
             else:
-                print("Normal request", log_attributes["http_url"])
+                pass  # print("Normal request", log_attributes["http_url"])
 
+    # Write to a file for use in cloudflare to block the ips
+    # (For use here: https://i.cmyui.xyz/twZwfdhmIys.png)
     with open(".data/ddos_ips.txt", "w") as f:
-        for ip in ips:
+        for ip in deduplicated_ips:
             f.write(f"{ip}\n")
 
-    print(f"Wrote {len(ips)} IPs to .data/ddos_ips.txt")
+    print(f"Wrote {len(deduplicated_ips)} IPs to .data/ddos_ips.txt")
     if failed_to_parse:
         print(f"Failed to parse {failed_to_parse} lines")
         return 1
